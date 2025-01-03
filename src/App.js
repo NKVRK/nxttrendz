e3f97f22722cfcf504d2344d6bac38065e55b95d
NKVRK <168123959+NKVRK@users.noreply.github.com>
2025-01-03 15:56:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
@@ -18,38 +18,82 @@ class App extends Component {
   }
 
   addCartItem = product => {
-    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    const {cartList} = this.state
+    const {id} = product
+    if (cartList !== []) {
+      console.log('entered')
+      const item = cartList.find(eachItem => eachItem.id === id)
+      console.log(item)
+      if (item === undefined) {
+        this.setState(prevState => ({
+          cartList: [...prevState.cartList, product],
+        }))
+      } else {
+        item.quantity += product.quantity
+        this.setState({cartList})
+      }
+    } else {
+      this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    }
   }
 
-  deleteCartItem = () => {}
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const newCartList = cartList.filter(eachItem => eachItem.id !== id)
+    this.setState({cartList: newCartList})
+  }
+
+  incrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const item = cartList.find(eachItem => eachItem.id === id)
+    item.quantity += 1
+    this.setState({cartList})
+  }
+
+  decrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const item = cartList.find(eachItem => eachItem.id === id)
+    if (item.quantity > 1) {
+      item.quantity -= 1
+      this.setState({cartList})
+    } else {
+      const newCartList = cartList.filter(eachItem => eachItem.id !== id)
+      this.setState({cartList: newCartList})
+    }
+  }
+
+  removeAllCartItems = () => {
+    this.setState({cartList: []})
+  }
 
   render() {
     const {cartList} = this.state
 
     return (
-      <BrowserRouter>
-        <CartContext.Provider
-          value={{
-            cartList,
-            addCartItem: this.addCartItem,
-            deleteCartItem: this.deleteCartItem,
-          }}
-        >
-          <Switch>
-            <Route exact path="/login" component={LoginForm} />
-            <ProtectedRoute exact path="/" component={Home} />
-            <ProtectedRoute exact path="/products" component={Products} />
-            <ProtectedRoute
-              exact
-              path="/products/:id"
-              component={ProductItemDetails}
-            />
-            <ProtectedRoute exact path="/cart" component={Cart} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect to="not-found" />
-          </Switch>
-        </CartContext.Provider>
-      </BrowserRouter>
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
+          removeAllCartItems: this.removeAllCartItems,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
     )
   }
 }
